c76f98e6c05f6f962daf727a963b7334cbc7c445
LinuxUser404 <nikolay.pershyn@gmail.com>
2020-12-25 03:14:58 +0200

    refactor: use applicative

--- src/DominionParser.hs
+++ src/DominionParser.hs
@@ -7,103 +7,52 @@ Program: Dominion Client
 module DominionParser (parseNotification) where
 
 import Text.ParserCombinators.Parsec
+import Text.Parsec.Prim (ParsecT)
+import Data.Functor.Identity (Identity)
 import DominionTypes (Notification(..), State(..), Move(..), Play(..), Name, Deck, readPlay)
 import DominionCards (readCard)
 
 -- Notification parser implementation
 myWords :: GenParser Char st [String]
-myWords = do
-  str <- many (noneOf "()")
-  return $ words str
-
+myWords = words <$> many (noneOf "()")
 
 parseNotification :: GenParser Char st DominionTypes.Notification
 parseNotification = spaces >>
   ((StateNotification <$> parseState) <|> (MoveNotification <$> parseMove))
 
-
 parseState :: GenParser Char st DominionTypes.State
-parseState = do
-  _ <- char '(';
-  spaces;
-  _ <- string "move"
-  spaces
-  _ <- char '('
-  spaces
+parseState = braces $ string "move" *> spaces *> do
+  char '(' *> spaces
   players'  <- parsePlayers
-  spaces
-  supply'   <- parseDeck   "supply"
-  spaces
-  trash'    <- parseDeck   "trash"
-  spaces
-  actions'  <- parsePoints "actions"
-  spaces
-  buys'     <- parsePoints "buys"
-  spaces
-  coins'    <- parsePoints "coins"
-  spaces
-  deck'     <- parseDeck   "deck"
-  spaces
-  hand'     <- parseDeck   "hand"
-  spaces
-  plays'    <- parseDeck   "plays"
-  spaces
-  discards' <- parseDeck   "discards"
-  spaces
-  _ <- char ')'
-  spaces;
-  _ <- char ')';
+  supply'   <- spaces *> parseDeck   "supply"
+  trash'    <- spaces *> parseDeck   "trash"
+  actions'  <- spaces *> parsePoints "actions"
+  buys'     <- spaces *> parsePoints "buys"
+  coins'    <- spaces *> parsePoints "coins"
+  deck'     <- spaces *> parseDeck   "deck"
+  hand'     <- spaces *> parseDeck   "hand"
+  plays'    <- spaces *> parseDeck   "plays"
+  discards' <- spaces *> parseDeck   "discards"
+  spaces <* char ')'
   return $ DominionTypes.State players' supply' trash' actions' buys' coins' deck' hand' plays' discards'
 
-
 parseDeck :: String -> GenParser Char st Deck
-parseDeck deckName = do
-  _ <- char '('
-  spaces
-  _ <- string deckName
-  temp <- myWords
-  spaces
-  _ <- char ')'
-  return $ map readCard temp
+parseDeck deckName = braces $ map readCard <$> (string deckName *> myWords)
 
 parsePoints :: String -> GenParser Char st Int
-parsePoints pointsName = do
-  _ <- char '('
-  spaces
-  _ <- string pointsName
-  spaces
-  temp <- many (noneOf "( )\n")
-  spaces
-  _ <- char ')'
-  return $ read temp
-
+parsePoints pointsName = braces $ read <$> (string pointsName *> spaces *> many (noneOf "( )\n"))
 
 parsePlayers :: GenParser Char st [Name]
-parsePlayers = do
-  _ <- char '('
-  spaces
-  _ <- string "players"
-  temp <- myWords
-  _ <- char ')'
-  return temp
-
+parsePlayers = braces $ string "players" *> myWords
 
 parseMove :: GenParser Char st DominionTypes.Move
-parseMove = do
-  _ <- char '('
-  spaces
-  _ <- string "moved"
-  spaces
+parseMove = braces $ do
+  string "moved" *> spaces
   name' <- many (noneOf "( )\n")
-  spaces
-  play' <- parsePlay
-  spaces
-  _ <- char ')'
-  return $ DominionTypes.Move name' play'
+  DominionTypes.Move name' <$> (spaces *> parsePlay)
 
 parsePlay :: GenParser Char st DominionTypes.Play
-parsePlay = do
-  _ <- char '('
-  temp <- myWords
-  _ <- char ')'
-  return $ readPlay temp
+parsePlay = braces $ readPlay <$> myWords
+
+braces :: ParsecT String u Identity a -> ParsecT String u Identity a
+braces = try . between (char '(' *> spaces) (spaces <* char ')')
